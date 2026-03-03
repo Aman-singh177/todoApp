c5ea3eaf5449e45414f0247a99be3d7d475e77d5
Aman-singh177 <amansingh17730@gmail.com>
2026-03-03 19:56:32 +0530

    commiting the todoApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const clickhandler = () => {
+    setTasks([...tasks, value]);
+    setValue("");
+  }
+  const handlechange = (event) => {
+    setValue(event.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Add task </h1> 
+      <div className="inputbox">
+        <input type="text" placeholder="Enter your task here" onChange={handlechange} value={value} />
+        <h1 className="addtask" onClick={clickhandler}> + </h1>
+      </div>
+      <div className="tasklist">
+        <h2> Your Tasks </h2>
+        <ul>
+          {
+            tasks.map((task, index) => (
+              
+                <li key={index}> {task} </li>
+              
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
 }
